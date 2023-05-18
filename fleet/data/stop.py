f1b6a441dee7eb224c20e25caf1e15ab68c708e9
Jiri Zak <jiri.zak@bringauto.com>
2023-05-18 15:45:08 +0200

    Validate floats

--- fleet/data/stop.py
+++ fleet/data/stop.py
@@ -6,7 +6,7 @@ class Stop:
     __slots__ = ('latitude', 'longitude', 'order', 'station_name')
 
     def __init__(self, latitude: float, longitude: float, order: int, station_name: str) -> None:
-        self.latitude = latitude
-        self.longitude = longitude
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
         self.order = order
         self.station_name = station_name
